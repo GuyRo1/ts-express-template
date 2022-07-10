3e9fb124abd2f6e1d60ee12dd94d5c9e786e7308
GuyRo1 <Guyrossert@gmail.com>
2022-07-10 13:05:04 +0300

    no top level await for now

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,3 @@ loadDependencies()
         process.exit(1)
     })
 
-// try {
-//     const dependencies = await loadDependencies()
-//     const app = createApp(routers, dependencies)
-//     const server: http.Server = http.createServer(app)
-//     server.listen(port, () => {
-//         console.log(`listening on port ${port}`);
-//     })
-// } catch (err) {
-//     console.log(err);
-//     process.exit(1)
-// }
